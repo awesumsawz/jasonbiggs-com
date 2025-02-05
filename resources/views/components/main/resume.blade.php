4fbde39d250d6be4b21a3f4bdf6668cb183698ba
Jason Biggs <development@jason-biggs.com>
2025-02-05 13:33:06 -0600

    started resume blade

--- resources/views/components/main/resume.blade.php
+++ resources/views/components/main/resume.blade.php
@@ -1,6 +1,31 @@
 <main class="{{ $mainClass }}">
-    <div>
-        <h1>{{ $title }}</h1>
-        {{ $content }}
-    </div>
+    <section class="intro">
+        <div class="title">
+            <h1>{{$title}}</h1>
+            <p class="subtitle">{{$subtitle}}</p>
+        </div>
+        <div class="content">
+            <div class="body-content">
+                <div class="post page type-page status-publish hentry">
+                    <div class="post-content">
+                        <div class="entry">
+                            <div class="wp-block-file aligncenter">
+                                <a href="{{$downloadURL}}" class="wp-block-file__button wp-element-button" download="">
+                                    Download a copy
+                                </a>
+                            </div>
+                            {{$introBlurb}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <hr>
+    <section class="education">
+        
+    </section>
+    <section class="skills"></section>
+    <section class="professional-exp"></section>
+    <section class="personal-exp"></section>
 </main>
--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -1,11 +1,33 @@
 <x-layout>
-	<x-slot:title>
-		Resume
-	</x-slot:title>
-	<x-slot:main-class>
-		front-page
-	</x-slot:main-class>
-	<x-slot:content>
-		Start small, Think Bigg
-	</x-slot:content>
+	<x-slot:main-class>resume-template</x-slot:main-class>
+	<x-slot:title>Resume</x-slot:title>
+	<x-slot:subtitle>Last Updated December 2024</x-slot:subtitle>
+	<x-slot:downloadURL>
+		{{ asset('files/12-05-24_JasonBiggs_Resume__WebDeveloper.pdf') }}
+	</x-slot:downloadURL>
+	<x-slot:intro-blurb>
+		<p>Jason’s journey into technology and development started after leaving a career in education, where he honed his skills in communication and problem-solving. At Judson University, he dove into system administration and customer support, while also putting his knack for web and graphic design to good use.</p>
+		<p>From there, he joined The Mx Group, blending customer-facing work with scripting and automation, creating tools with PowerShell, Bash, and Python that streamlined workflows and boosted productivity. This sparked his passion for development, leading him to Dealer Inspire, where he quickly transitioned from Trainer to Full-Stack Developer. Over the years, he built and maintained websites, extended plugins, and created tools that made a real impact. After exiting DI amid a restructuring in early 2024, Jason took the opportunity to learn React, Next.js, and TypeScript, expanding his front-end expertise.</p>
+		<p>Now, as a Senior Software Engineer at Chime Financial, Jason helps drive revenue and user engagement by building dynamic pages, troubleshooting WordPress, and creating tools with PHP, JavaScript, and Advanced Custom Fields. His cross-departmental documentation efforts ensure workflows are smooth and knowledge is shared effectively across teams.</p>
+		<p>Outside of work, Jason loves spending time with his daughter Eleanor, exploring new hobbies (hello, coffee brewing and photography), and tinkering with his home network and gaming computer. He’s a tech enthusiast, podcast aficionado (Accidental Tech Podcast, Connected, The Vergecast), and an actual-play podcast superfan (The Adventure Zone, Dungeons & Daddies, Dimension 20).</p>
+	</x-slot:intro-blurb>
+	<x-slot:education>
+		<div class="title">
+			<h2>Education</h2>
+		</div>
+		<div class="content">
+			<div class="degree">
+				<h3>Master of Science</h3>
+				<p class="focus">Education in Literacy</p>
+				<p>June 2016</p>
+				<p>Judson University</p>
+				<p>Elgin, Illinois</p>
+			</div>
+			<div class="degree">
+				<h3>Bachelor of Science</h3>
+				<p class="focus">Elementary Education</p>
+				<p>May 2013</p>
+				<p>Northern Illinois University</p>
+				<p>DeKalb, Illinois</p>
+			</div></div>
 </x-layout> 
