4bd7f2c93cdb321d47f0685b805f3f294a0b1392
Jason Biggs <development@jason-biggs.com>
2025-02-05 13:31:45 -0600

    format

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
                 'resources/css/app.css', 
                 'resources/css/style.css',
                 'resources/js/app.js',
-             ],
+            ],
             refresh: true,
         }),
     ],
