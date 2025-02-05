e21878d68e5dd73480b8ab2a9f14d41d9fe22788
Jason Biggs <development@jason-biggs.com>
2025-02-05 13:32:34 -0600

    built out the web view

--- resources/views/web.blade.php
+++ resources/views/web.blade.php
@@ -1,11 +1,229 @@
-<x-layout>
+@php
+	$galleryCards = [
+		// PLATO
+		[
+			"title" => "Plato 01",
+			"description" => "",
+			"date" => "09/2023",
+			"imageURL" => asset('images/gallery/webdev-plato-01.jpg'),
+			"imageAlt" => "Plato 01"
+		],
+		[
+			"title" => "Plato 02",
+			"description" => "",
+			"date" => "09/2023",
+			"imageURL" => asset('images/gallery/webdev-plato-02.jpg'),
+			"imageAlt" => "Plato 02"
+		],
+		// PLAYBOOK
+		[
+			"title" => "Playbook 01",
+			"description" => "",
+			"date" => "11/2024",
+			"imageURL" => asset('images/gallery/webdev-playbook-01.jpg'),
+			"imageAlt" => "Playbook 01"
+		],
+		[
+			"title" => "Playbook 02",
+			"description" => "",
+			"date" => "11/2024",
+			"imageURL" => asset('images/gallery/webdev-playbook-02.jpg'),
+			"imageAlt" => "Playbook 02"
+		],
+		// TRAINING CAMP
+		[
+			"title" => "Training Camp 01",
+			"description" => "",
+			"date" => "11/2024",
+			"imageURL" => asset('images/gallery/webdev-training_camp-01.jpg'),
+			"imageAlt" => "Training Camp 01"
+		],
+		[
+			"title" => "Training Camp 02",
+			"description" => "",
+			"date" => "11/2024",
+			"imageURL" => asset('images/gallery/webdev-training_camp-02.jpg'),
+			"imageAlt" => "Training Camp 02"
+		],
+		[
+			"title" => "Training Camp 03",
+			"description" => "",
+			"date" => "11/2024",
+			"imageURL" => asset('images/gallery/webdev-training_camp-03.jpg'),
+			"imageAlt" => "Training Camp 03"
+		],
+		[
+			"title" => "Training Camp 04",
+			"description" => "",
+			"date" => "11/2024",
+			"imageURL" => asset('images/gallery/webdev-training_camp-04.jpg'),
+			"imageAlt" => "Training Camp 04"
+		],
+		// THINK BIGG
+		[
+			"title" => "Think Bigg 01",
+			"description" => "",
+			"date" => "11/2022",
+			"imageURL" => asset('images/gallery/webdev-think_bigg-01.png'),
+			"imageAlt" => "Think Bigg 01"
+		],
+		[
+			"title" => "Think Bigg 02",
+			"description" => "",
+			"date" => "11/2022",
+			"imageURL" => asset('images/gallery/webdev-think_bigg-02.png'),
+			"imageAlt" => "Think Bigg 02"
+		],
+		[
+			"title" => "Think Bigg 03",
+			"description" => "",
+			"date" => "11/2022",
+			"imageURL" => asset('images/gallery/webdev-think_bigg-03.png'),
+			"imageAlt" => "Think Bigg 03"
+		],
+		[
+			"title" => "Think Bigg 04",
+			"description" => "",
+			"date" => "11/2022",
+			"imageURL" => asset('images/gallery/webdev-think_bigg-04.png'),
+			"imageAlt" => "Think Bigg 04"
+		],
+		// TECHREGULAR
+		[
+			"title" => "TechRegular 01",
+			"description" => "",
+			"date" => "11/2022",
+			"imageURL" => asset('images/gallery/webdev-techregular-01.png'),
+			"imageAlt" => "TechRegular 01"
+		],
+		[
+			"title" => "TechRegular 02",
+			"description" => "",
+			"date" => "11/2022",
+			"imageURL" => asset('images/gallery/webdev-techregular-02.png'),
+			"imageAlt" => "TechRegular 02"
+		],
+		[
+			"title" => "TechRegular 03",
+			"description" => "",
+			"date" => "11/2022",
+			"imageURL" => asset('images/gallery/webdev-techregular-03.png'),
+			"imageAlt" => "TechRegular 03"
+		],
+		[
+			"title" => "TechRegular 04",
+			"description" => "",
+			"date" => "11/2022",
+			"imageURL" => asset('images/gallery/webdev-techregular-04.png'),
+			"imageAlt" => "TechRegular 04"
+		],
+		[
+			"title" => "TechRegular 05",
+			"description" => "",
+			"date" => "11/2022",
+			"imageURL" => asset('images/gallery/webdev-techregular-05.png'),
+			"imageAlt" => "TechRegular 04"
+		],
+		[
+			"title" => "TechRegular 06",
+			"description" => "",
+			"date" => "11/2022",
+			"imageURL" => asset('images/gallery/webdev-techregular-06.png'),
+			"imageAlt" => "TechRegular 04"
+		],
+		[
+			"title" => "TechRegular 07",
+			"description" => "",
+			"date" => "11/2022",
+			"imageURL" => asset('images/gallery/webdev-techregular-07.png'),
+			"imageAlt" => "TechRegular 04"
+		],
+		// LITCUP
+		[
+			"title" => "Lit Cup 01",
+			"description" => "",
+			"date" => "11/2024",
+			"imageURL" => asset('images/gallery/webdev-litcup-01.png'),
+			"imageAlt" => "Lit Cup 01"
+		],
+		[
+			"title" => "Lit Cup 02",
+			"description" => "",
+			"date" => "11/2024",
+			"imageURL" => asset('images/gallery/webdev-litcup-02.png'),
+			"imageAlt" => "Lit Cup 02"
+		],
+		[
+			"title" => "Lit Cup 03",
+			"description" => "",
+			"date" => "11/2024",
+			"imageURL" => asset('images/gallery/webdev-litcup-03.png'),
+			"imageAlt" => "Lit Cup 03"
+		],
+		// JUDSON UNIVERSITY NEWS
+		[
+			"title" => "Judson University News 01",
+			"description" => "",
+			"date" => "11/2022",
+			"imageURL" => asset('images/gallery/webdev-judson-01.png'),
+			"imageAlt" => "Judson University News 01"
+		],
+		[
+			"title" => "Judson University News 02",
+			"description" => "",
+			"date" => "11/2022",
+			"imageURL" => asset('images/gallery/webdev-judson-02.png'),
+			"imageAlt" => "Judson University News 01"
+		],
+		[
+			"title" => "Judson University News 03",
+			"description" => "",
+			"date" => "11/2022",
+			"imageURL" => asset('images/gallery/webdev-judson-03.png'),
+			"imageAlt" => "Judson University News 01"
+		],
+		[
+			"title" => "Judson University News 04",
+			"description" => "",
+			"date" => "11/2022",
+			"imageURL" => asset('images/gallery/webdev-judson-04.png'),
+			"imageAlt" => "Judson University News 01"
+		],
+	];
+@endphp
+
+<x-layout :galleryCards="$galleryCards">
 	<x-slot:title>
-		Jason Biggs Developer
+		Web Wizard
 	</x-slot:title>
 	<x-slot:main-class>
-		front-page
+		showcase-template
 	</x-slot:main-class>
-	<x-slot:content>
-		Start small, Think Bigg
-	</x-slot:content>
+	<x-slot:intro-blurb>
+		<p>Jason’s love for web development started while exploring HTML and CSS on Codecademy during his time as an educator. What began as a hobby—creating websites for friends and family—quickly turned into a passion for building meaningful digital experiences.</p>
+		<p>Now an experienced developer, Jason specializes in tools like JavaScript (in all its various flavors), PHP, and WordPress, combining technical expertise with creative problem-solving to bring ideas to life and deliver solutions that make an impact.</p>
+	</x-slot:intro-blurb>
+	<x-slot:examples-column-one>
+		<div class="title">
+			<h2>Development Examples</h2>
+		</div>
+		<div class="links">
+			<div class="link"><a href="https://github.com/awesumsawz/TechRegular-Blog">NextJS Blog Repo</a></div>
+			<div class="link"><a href="https://github.com/awesumsawz/MHU-Gravity-Forms-Extended">Wordpress Plugin Sample</a></div>
+			<div class="link"><a href="https://github.com/awesumsawz">Github</a></div>
+			<div class="link"><a href="https://web.jason-biggs.com/index.html">Web Development Examples (Updated 2019)</a></div>
+		</div>
+	</x-slot:examples-column-one>
+	<x-slot:examples-column-two>
+		<div class="title">
+			<h2>Websites in Production</h2>
+		</div>
+		<div class="links">
+			<div class="link"><a href="http://www.techregular.net">TechRegular Blog<br/>NextJS & Contentful - AWS</a></div>
+			<div class="link"><a href="https://modelhorseuniversity.com/">Model Horse University<br/>Wordpress - Siteground</a></div>
+			<div class="link"><a href="https://litcup.net">Lit Cup (SquareSpace)</a></div>
+			<div class="link"><a href="https://training.dealerinspire.com">Training Camp</a></div>
+			<div class="link"><a href="https://playbook.dealerinspire.com">Dealer Inspire Playbook</a></div>	
+		</div>
+	</x-slot:examples-column-two>
 </x-layout> 
