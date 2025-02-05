4bff892a4529b36e214357e36a5b5d86d6cbdbfa
Jason Biggs <development@jason-biggs.com>
2025-02-05 13:32:46 -0600

    removed leading forward slashes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,13 +3,19 @@
 
 @if (Request::is('/'))
     @include('components.main.home')
-@elseif (Request::is('/web'))
+@elseif (Request::is('web'))
     @include('components.main.web')
-@elseif (Request::is('/resume'))
+@elseif (Request::is('resume'))
     @include('components.main.resume')
-@elseif (Request::is('/blog'))
-    @include('components.main.blog')
+@elseif (Request::is('blog'))
+    @php echo "blog"; @endphp
+@elseif (Request::is('tech'))
+    @include('components.main.tech')
 @else
+    @php 
+    echo "404"; 
+    print_r(Request::path());
+    @endphp
     @include('components.main.404')
 @endif
 
