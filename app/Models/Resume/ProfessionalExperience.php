ba09bc3e1a5cb831434bbe1ae89709fb6abcc39b
Jason Biggs <development@jason-biggs.com>
2025-02-08 13:28:27 -0600

    build a resume model for professional experience

---
+++ app/Models/Resume/ProfessionalExperience.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Resume;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProfessionalExperience extends Model
+{
+	protected $table = 'professional_experiences';
+	protected $fillable = ['position', 'company', 'start_date', 'end_date', 'location', 'description'];
+}
