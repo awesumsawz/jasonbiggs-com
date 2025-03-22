7635997ca11f6686045a59a0197e17ebbc15bcb0
Jason Biggs <development@jason-biggs.com>
2025-03-22 07:58:50 -0500

    update blog styles

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -13,7 +13,7 @@
 
     <header class="post-header">
         <h1 class="post-title">{{ $post['metadata']['title'] }}</h1>
-        
+
         @if(isset($post['metadata']['tags']) && is_array($post['metadata']['tags']) && count($post['metadata']['tags']) > 0)
             <div class="post-tags">
                 <div class="tags-wrapper">
@@ -25,23 +25,25 @@
                 </div>
             </div>
         @endif
-        
+
         <div class="post-meta">
-            <span class="post-date">{{ $post['metadata']['formatted_date'] ?? date('F j, Y', strtotime($post['metadata']['date'])) }}</span> 
+            <span class="post-date">{{ $post['metadata']['formatted_date'] ?? date('F j, Y', strtotime($post['metadata']['date'])) }}</span>
             <span class="reading-time">• {{ $post['metadata']['reading_time'] }} min read</span>
         </div>
     </header>
-    
+
     @if(isset($post['metadata']['featured_image']))
         <div class="post-featured-image">
             <img src="{{ $post['metadata']['featured_image'] }}" alt="{{ $post['metadata']['title'] }}">
         </div>
+    @else
+        <hr>
     @endif
-    
+
     <div class="post-content">
         {!! $post['content'] !!}
     </div>
-    
+
     <div class="post-footer">
         <hr class="my-8 border-light-gray dark:border-gray">
         <div class="post-navigation">
@@ -56,7 +58,7 @@
             @else
                 <div class="nav-placeholder"></div>
             @endif
-            
+
             @if(isset($nextPost))
                 <a href="{{ route('blog.show', $nextPost['slug']) }}" class="next-post">
                     <div class="nav-header">
@@ -71,4 +73,4 @@
         </div>
     </div>
 </article>
-@endsection 
+@endsection
